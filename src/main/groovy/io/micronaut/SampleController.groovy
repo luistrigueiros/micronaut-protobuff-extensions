11bd1ffa14f25ea65f7c9e7cbdc4862523d0c76e
Luis Trigueiros <luistrigueiros@gmail.com>
2018-12-29 01:04:58 +0000

    Added @Consumes(ProtobufferCodec.PROTOBUFFER_ENCODED) to the routes

--- src/main/groovy/io/micronaut/SampleController.groovy
+++ src/main/groovy/io/micronaut/SampleController.groovy
@@ -2,6 +2,7 @@ package io.micronaut
 
 import com.example.wire.Example
 import groovy.transform.CompileStatic
+import io.micronaut.http.annotation.Consumes
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
@@ -12,11 +13,13 @@ import io.micronaut.http.codec.ProtobufferCodec
 @CompileStatic
 class SampleController {
     @Get(value = "/city", produces = ProtobufferCodec.PROTOBUFFER_ENCODED)
+    @Consumes(ProtobufferCodec.PROTOBUFFER_ENCODED)
     Example.GeoPoint city() {
         Constant.DUBLIN
     }
 
     @Post(value = "/nearby", produces = ProtobufferCodec.PROTOBUFFER_ENCODED)
+    @Consumes(ProtobufferCodec.PROTOBUFFER_ENCODED)
     Example.GeoPoint suggestVisitNearBy(Example.GeoPoint point) {
         Constant.DUBLIN
     }
